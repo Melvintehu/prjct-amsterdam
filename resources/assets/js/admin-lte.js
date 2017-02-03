3f9dfa0c7c82b52ce1c6f7da88c0f1076b1d2936
Melvin Tehubijuluw <melvin.tehubijuluw@beeproger.com>
2017-02-03 19:04:40 +0100

    Update
    Seperated the header and sidebar in partials
    updated the gitignore file
    Admin-lte is now in a seperate file

---
+++ resources/assets/js/admin-lte.js
@@ -0,0 +1,26 @@
+require('jquery');
+require('bootstrap-sass');
+require('fastclick');
+require('jquery-slimscroll');
+require('admin-lte');
+require('admin-lte/plugins/select2/select2.full.min');
+require('admin-lte/plugins/input-mask/jquery.inputmask');
+require('admin-lte/plugins/input-mask/jquery.inputmask.date.extensions');
+require('admin-lte/plugins/input-mask/jquery.inputmask.extensions');
+require('admin-lte/plugins/daterangepicker/daterangepicker');
+require('admin-lte/plugins/datepicker/bootstrap-datepicker');
+require('admin-lte/plugins/colorpicker/bootstrap-colorpicker.min');
+require('admin-lte/plugins/timepicker/bootstrap-timepicker.min');
+require('admin-lte/plugins/iCheck/icheck.min');
+
+
+// init plugins here
+$('.datepicker').datepicker({
+  format: 'yyyy-mm-dd'
+});
+$('.timepicker').timepicker({
+  template: 'dropdown',
+  showInputs: false,
+  maxHours: 24,
+  showMeridian: false
+});
